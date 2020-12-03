4dd833129d2566fc2f20773bc36b0e4c3de3af10
prajaktabramhe <prajakta14bramhe@gmail.com>
2020-12-03 22:52:21 +0530

    Added while-loop program files

---
+++ while_loop/Magic_Number.js
@@ -0,0 +1,23 @@
+function magic_Num(number){
+    if ( number >= 1 && number <= 100){
+       while (number > 0){ 
+	    let mid = (lowerLimit + (upperLimit - lowerLimit) / 2 ); 
+        if (number < mid){
+            upperLimit = ( mid - 1 );
+        }
+        else if (number > mid){  
+            lowerLimit = ( mid + 1 );
+        }
+        else{ 
+	        console.log("The number you guessed is "+number);
+            break;
+        }
+    }
+}
+else 
+console.log("The number is out of range.");
+}
+let lowerLimit = 1;
+let upperLimit = 100;
+let number = 22; 
+magic_Num(number);
---
+++ while_loop/power_Of_two.js
@@ -0,0 +1,11 @@
+function powerOfTwo(NUMBER){
+    for (let index=0; index<=limit; index++) {
+        let power = (Math.pow(NUMBER, index));
+    if(power < 256 ){
+        console.log(power)
+    }
+}
+   }
+   const NUMBER = 2;
+   let limit = 12;
+   powerOfTwo(NUMBER);
