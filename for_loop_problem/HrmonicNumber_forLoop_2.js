1d84553f45e7111d0fb9dcd40964dce78f0c47df
prajaktabramhe <prajakta14bramhe@gmail.com>
2020-12-03 21:38:25 +0530

    added forloop Program Files

---
+++ for_loop_problem/HrmonicNumber_forLoop_2.js
@@ -0,0 +1,15 @@
+function HarmonicSeries(num) 
+{
+    let sum = 0;
+    console.log(" Harmonic Series :");
+    for (let i = 1; i <= num; i++) 
+    {
+      console.log("1/" + i + "+");
+      sum = sum + 1 / i; //computes the  sum of harmonic series upto given number
+    }
+    console.log("=" + sum.toFixed(2));
+}
+  
+  HarmonicSeries(10);
+
+  
